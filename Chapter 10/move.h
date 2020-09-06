cee167a84606bf9b3376edd6d7c85925543283f3
barcarolle <42168566+barcarolle@users.noreply.github.com>
2020-09-06 10:47:55 +0900

    Create move.h

---
+++ Chapter 10/move.h
@@ -0,0 +1,11 @@
+class Move
+{
+private:
+    double x;
+    double y;
+public:
+    Move(double a = 0; double b = 0);
+    showmove() const;
+    Move add(const Move & m) const;
+    reset(double a = 0; double b = 0);
+};
