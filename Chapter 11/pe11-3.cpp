3c3e74632ee7467262d96147ddf85ec412ed0ec2
barcarolle <42168566+barcarolle@users.noreply.github.com>
2020-09-11 20:26:49 +0900

    Create pe11-3.cpp

---
+++ Chapter 11/pe11-3.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "vect.h"
+
+int main()
+{
+  using namespace std;
+  using VECTOR::Vector;
+  srand(time(0));
+  double direction;
+  Vector step;
+  Vector result(0.0, 0.0);
+  unsigned long steps = 0;
+  double target;
+  double dstep;
+  int NUM, i;
+  
+  cout << "Enter an integer you want: ";
+  cin >> NUM;
+  
+  int combination[NUM];
+  
+  cout << "Enter target distance (q to quit): ";
+  cin >> target;
+  
+  cout << "Enter step length: ";
+  if (!(cin >> dstep))
+    break;
+    
+  for (i = 0; i < NUM; i++)
+  {
+    while (result.magval() < target)
+    {
+      direction = rand() % 360;
+      step.reset(dstep, direction, Vector::POL);
+      result = result + step;
+      steps++;
+    }
+    
+    combination[i] = steps;
+    
+  }
+  
+  
+  
+  
+  
+
+  
+  
+  
+  
+  
