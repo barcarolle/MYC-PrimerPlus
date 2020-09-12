74cef00f3450bc554f73dac4cd56a70659c7a7f7
barcarolle <42168566+barcarolle@users.noreply.github.com>
2020-09-12 17:20:05 +0900

    Create mytime4.h

---
+++ Chapter 11/mytime4.h
@@ -0,0 +1,23 @@
+#ifndef MYTIME4_H_
+#define MYTIME4_H_
+#include <iostream>
+
+class Time
+{
+private:
+  int hours;
+  int minutes;
+  
+public:
+  Time();
+  Time(int h, int m = 0);
+  void AddMin(int m);
+  void AddHr(int h);
+  void Reset(int h = 0, int m = 0);
+  friend Time operator+
+  friend Time operator-
+  friend Time operator*
+  friend std::ostream & operator<<(std::ostream & os, const Time & t);
+};
+
+endif
