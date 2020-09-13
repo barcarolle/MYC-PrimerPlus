1083c14cebffb06990ead831a01ae01e46a6beda
barcarolle <42168566+barcarolle@users.noreply.github.com>
2020-09-13 11:26:01 +0900

    Create stonewt2.cpp

---
+++ Chapter 11/stonewt2.cpp
@@ -0,0 +1,17 @@
+Stonewt(double lbs)
+{
+  pounds = lbs;
+}
+
+
+Stonewt(double lbs, char st)
+{
+  using std::cin;
+  
+  if ((cin >> st) = '2')
+    pounds = int (lbs);
+    
+  else if ((cin >> st) = '3')
+    pounds = float (lbs);
+}
+
