51354c557bf8a3c519ac7812bfa423108675ca0f
barcarolle <42168566+barcarolle@users.noreply.github.com>
2020-09-15 18:42:03 +0900

    Create pe11-6.cpp

---
+++ Chapter 11/pe11-6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "stonewt3.h"
+
+int main()
+{
+  Stonewt stone[6] = {
+    Stonewt(10, 0.5);
+    Stonewt(20, 0.5);
+    Stonewt(30, 0.5);
+    };
+    
+    cout << Stonewt[1];
+    
+    return 0;
+
+}
+  
+  
